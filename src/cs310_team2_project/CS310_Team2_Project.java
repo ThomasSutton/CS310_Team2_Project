f632a26c621d1d996dd4234cb2bb4f4c486f9397
CS310Team2 <cs310teamtwo@gmail.com>
2017-11-06 11:25:55 -0600

    "Completed Feature 3"

--- src/cs310_team2_project/CS310_Team2_Project.java
+++ src/cs310_team2_project/CS310_Team2_Project.java
@@ -3,15 +3,11 @@ package cs310_team2_project;
 public class CS310_Team2_Project {
     public static void main(String[] args){
         TASDatabase db = new TASDatabase();
-        Badge b = db.getBadge("12565C60");
-        System.out.println(b.toString());
-        Shift s = db.getShift(2);
-        System.out.println(s.toString());
-        Punch p = db.getPunch(3433);
-        System.out.println(p.printOriginalTimestamp());
         
-        Punch p2 = new Punch("4E6E296E", 105, 1);
-        System.out.println(p2.printOriginalTimestamp());
-        System.out.println(Integer.toString(db.insertPunch(p2)));
+        Shift s1 = db.getShift(1);
+        System.out.println("\n" + s1.toString());
+        Punch p3 = db.getPunch(4119);
+        p3.adjust(s1);
+        System.out.println(p3.printAdjustedTimestamp());
     }
 }
